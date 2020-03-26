0d8150d043163c8fbe3ecc6635fc0217f4192efd
mescamez <escamezm@gmail.com>
2020-03-26 17:40:00 +0100

    Mitad de ejercicio

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -1,3 +1,4 @@
+
 #Example: 
 
 eggs = (1,3,8,3,2)
@@ -8,54 +9,51 @@ print(my_listComprehension)
 
 
 #Insert here the module/library import statements 
-   
 
-"""
+import math
+import random   
+
 #1. Calculate the square number of the first 20 numbers. Use square as the name of the list.
 # Remember to use list comprehensions and to print your results
 
-square=[e** for e in ]
-
-
+square=[e**2 for e in range(0,21) ]
+print(square)
 
 #2. Calculate the first 50 power of two. Use power_of_two as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
+power_of_two=[2**i for i in range(0, 51)]
+print(power_of_two)
 
 
 #3. Calculate the square root of the first 100 numbers. Use sqrt as the name of the list.
 # You will probably need to install math library with pip and import it in this file.  
 # Remember to use list comprehensions and to print your results
-
-
-
+sqrt=[math.sqrt(i) for i in range(0, 101)]
+print(sqrt)
 
 #4. Create this list [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0]. Use my_list as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+my_list=[ e for e in range(-10, 1)]
+print(my_list)
 
 #5. Find the odd numbers from 1-100. Use odds as the name of the list. 
 # Remember to use list comprehensions and to print your results
-
-
+odds=[e for e in range(0, 101) if e%2!=0]
+print(odds)
 
 
 #6. Find all of the numbers from 1-1000 that are divisible by 7. Use divisible_by_seven as the name of the list.
 # Remember to use list comprehensions and to print your results
-
-
-
+divisible_by_seven=[i for i in range(1, 1001) if i%7==0]
+print(divisible_by_seven)
 
 #7. Remove all of the vowels in a string. Hint: make a list of the non-vowels. Use non_vowels as the name of the list.
 # Remember to use list comprehensions and to print your results
 # You can use the following test string but feel free to modify at your convenience
-
+vowels=['a','e','i','o','u']
 teststring = 'Find all of the words in a string that are monosyllabic'
-
-
+non_vowels=[e for e in teststring if e not in vowels]
+print(non_vowels)
 
 
 #8. Find the capital letters (and not white space) in the sentence 'The Quick Brown Fox Jumped Over The Lazy Dog'. 
@@ -64,12 +62,9 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 
 
 
-
 #9. Find all the consonants in the sentence 'The quick brown fox jumped over the lazy dog'.
 # Use consonants as the name of the list.
 # Remember to use list comprehensions and to print your results.
-
-
 
 
 
@@ -82,15 +77,16 @@ teststring = 'Find all of the words in a string that are monosyllabic'
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
 # Remember to use list comprehensions and to print your results
-
-
+random_lists=[[e for e in random.choices(range(1,101), k=10)] for e in range(4)]
+print(random_lists)
 
 
 #12. Flatten the following list of lists. Use flatten_list as the name of the output.
 # Remember to use list comprehensions and to print your results
 
 list_of_lists = [[1,2,3],[4,5,6],[7,8,9]]
-
+flaten_lists=[[x for e in list_of_lists for x in e]]
+print(flaten_lists)
 
 
 #13. Convert the numbers of the following nested list to floats. Use floats as the name of the list. 
@@ -189,4 +185,4 @@ def linux_interaction():
 Total_Marks = int(input("Enter Total Marks Scored: ")) 
 Num_of_Sections = int(input("Enter Num of Sections: "))
 
-"""
+
