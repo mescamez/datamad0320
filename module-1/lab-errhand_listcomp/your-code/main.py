7c8add9377a29a7cf822153a678ea543aebd0830
mescamez <escamezm@gmail.com>
2020-03-27 09:16:31 +0100

    comentarios

--- module-1/lab-errhand_listcomp/your-code/main.py
+++ module-1/lab-errhand_listcomp/your-code/main.py
@@ -86,6 +86,9 @@ for root, dirs, files in os.walk("/Users/manuelescamezsanchez/datamad0320", topd
 
 print (files)
 
+## Esto ha sido lo maximo que he conseguido aproximarme en este ejercicio, se que debería hacerlo como list comprehesion.
+## Además creo que el resultado no muestra exactamente lo que pide, pero no he conseguido avanzar más.
+
 
 #11. Create 4 lists of 10 random numbers between 0 and 100 each. Use random_lists as the name of the list. 
 #You will probably need to import random module
