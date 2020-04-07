badcdbba5173a9a8ddcf02fb80841a58c5ceb01d
mescamez <escamezm@gmail.com>
2020-04-07 19:45:37 +0200

    lab-numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,69 +1,72 @@
+
 #1. Import the NUMPY package under the name np.
 
-
-
+import numpy as np
+"""
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
+a1 = np.random.rand(2,3,5)
+a2 = np.random.random_sample((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
+"""
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
-
+print(b)
+"""
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+a.size==b.size
 
 #8. Are you able to add a and b? Why or why not?
 
-
+print(np.concatenate(a,b))
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.reshape(2,3,5)
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a,c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(b)
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a*c
 
 #13. Does e equal to a? Why or why not?
 
-
-
+Son iguales, ya que estamos multiplicando todos los valores de a por 1
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty((2,3,5))
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -73,12 +76,38 @@ Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
-"""
 
+for e in d:
+    for x in e:
+        for y in x:
+            if y > d_min and y < d_mean:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c=25
+            elif y > d_mean and y < d_max:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c=75
+            elif y == d_mean:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c=50
+            elif y == d_min:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c=0
+            elif y == d_max:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c=100
+print(f)
 
-
-
-"""
+"
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
@@ -97,10 +126,10 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
-"""
 
+print(d)
+print(f)
 
-"""
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
@@ -111,4 +140,37 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
-"""
+
+for e in d:
+    for x in e:
+        for y in x:
+            if y > d_min and y < d_mean:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c='B'
+            elif y > d_mean and y < d_max:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c='D'
+            elif y == d_mean:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c='C'
+            elif y == d_min:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c='A'
+            elif y == d_max:
+                for a in f:
+                    for b in a:
+                        for c in b:
+                            c='E'
+        
+print(f)
+
+#En este caso he sustituido los valores del ejercicio anterior por las letras correspondientes
+#pero el resultado que me da no es correcto y no encuentro donde está el fallo.
