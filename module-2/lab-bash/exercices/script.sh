f87d072175edab86489ea4f84c650fe0fe668da9
mescamez <escamezm@gmail.com>
2020-04-07 17:40:04 +0200

    lab-bash

---
+++ module-2/lab-bash/exercices/script.sh
@@ -0,0 +1,43 @@
+#Ejercicio 1
+echo Hola Mundo
+#Ejercicio 2
+mkdir new_dir
+#Ejercicio 3
+rm -d -i new_dir
+#Ejercicio 4
+cp lorem/sed.txt lorem-copy
+#Ejercicio 5
+cat.txt
+#Ejercicio 6
+cp lorem/at.txt lorem-copy/; cp lorem/lorem.txt lorem-copy/
+#Ejercicio 7
+cat sed.txt | head -n3
+#Ejercicio 8
+cat sed.txt | tail -n3
+#Ejercicio 9
+echo "Homo homini lupus.">> sed.txt
+#Ejercicio 10
+cat sed.txt | tail -n3
+#Ejercicio 11
+sed 's/et/ET/g' sed.txt
+#Ejercicio 12
+who
+#Ejercicio 13
+pwd
+#Ejercicio 14
+find . -name "*.txt"
+#Ejercicio 15
+ls lorem | wc -l
+#Ejercicio 16
+grep -c et lorem/at.txt
+#Ejercicio 17
+grep -c "et" lorem/at.txt
+#Ejercicio 18
+grep -c "et" lorem-copy/*.txt
+
+
+
+
+
+
+ 
